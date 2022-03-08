1367cb653d2089088c4a6e525fcfc61eb055dfa5
otg <otg@sateoki.xyz>
2022-03-08 12:52:40 +0000

    Updated config.def.h for tatami patch

--- config.def.h
+++ config.def.h
@@ -62,10 +62,11 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #include "fibonacci.c"
+#include "tatami.c"
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -76,6 +77,7 @@ static const Layout layouts[] = {
     { "===",      bstackhoriz },
     { "[@]",      spiral },
     { "[\\]",     dwindle },
+    { "|+|",      tatami },
 };
 
 /* key definitions */
@@ -117,6 +119,7 @@ static Key keys[] = {
     { MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
     { MODKEY,                       XK_r,      setlayout,      {.v = &layouts[5]} },
     { MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[6]} },
+    { MODKEY,                       XK_y,      setlayout,      {.v = &layouts[7]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
