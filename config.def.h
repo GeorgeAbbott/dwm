3cef599124799b9f4af1f702a79f4f41f12e4b0f
self <self@sateoki.xyz>
2022-03-12 18:52:35 +0000

    Updated automove for librewolf discord

--- config.def.h
+++ config.def.h
@@ -73,15 +73,18 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
-    { NULL,		  "spterm",		NULL,		SPTAG(0),		1, 0, 1,			 -1 },
-    { NULL,		  "spfm",		NULL,		SPTAG(1),		1, 0, 1			 -1 },
-    { NULL,		  "keepassxc",	NULL,		SPTAG(2),		1, 0, 1			 -1 },
-    { NULL,       "spmp",  NULL,       SPTAG(3),       1, 0, 1,         -1 },
+	/* class      instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",      NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "LibreWolf", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "st",        NULL,     NULL,           0,         0,          1,           0,        -1 },
+    { "discord",   NULL,     NULL,           1 << 5,    0,          0,          -1,        -1 },
+
+    { NULL,        NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+   
+    { NULL,		   "spterm",		NULL,		SPTAG(0),		1, 0, 1,			 -1 },
+    { NULL,		   "spfm",		NULL,		SPTAG(1),		1, 0, 1			 -1 },
+    { NULL,		   "keepassxc",	NULL,		SPTAG(2),		1, 0, 1			 -1 },
+    { NULL,        "spmp",  NULL,       SPTAG(3),       1, 0, 1,         -1 },
 
 
 
