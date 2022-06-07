009a7679f50fd64dfc472b39a7fbf1e6eab0d4c2
self <self@sateoki.xyz>
2022-06-07 20:14:16 +0100

    Enacted corrective measures for a blunderous faux pas

--- config.def.h
+++ config.def.h
@@ -109,18 +109,18 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "LibreWolf", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-    { "librewolf", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "st",        NULL,     NULL,           0,         0,          1,           0,        -1 },
-    { "discord",   NULL,     NULL,           1 << 5,    0,          0,          -1,        -1 },
+	/* class      instance       title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "LibreWolf", NULL,         NULL,           1 << 8,    0,          0,          -1,        -1 },
+    { "librewolf", NULL,         NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "st",        NULL,         NULL,           0,         0,          1,           0,        -1 },
+    { "discord",   NULL,         NULL,           1 << 5,    0,          0,          -1,        -1 },
 
-    { NULL,        NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+    { NULL,        NULL,         "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
    
-    { NULL,		   "spterm", NULL,           SPTAG(0),  1,          0,           1,        -1 },
-    { NULL,		   "spfm",	 NULL,           SPTAG(1),  1,          0,           1,        -1 },
-    { NULL,		   "spkdbx", NULL,           SPTAG(2),  1,          0,           1,        -1 },
-    { NULL,        "spmp",   NULL,           SPTAG(3),  1,          0,           1,        -1 },
+    { NULL,		   "spterm",     NULL,           SPTAG(0),  1,          0,           1,        -1 },
+    { NULL,		   "spfm",	     NULL,           SPTAG(1),  1,          0,           1,        -1 },
+    { NULL,		   "keepassxc",  NULL,           SPTAG(2),  1,          0,           1,        -1 },
+    { NULL,        "spmp",       NULL,           SPTAG(3),  1,          0,           1,        -1 },
 
 
 
